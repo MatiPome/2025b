817d34601a16d92c24825f78bdfa69e16dffb12c
Matias <matiaspome95@gmail.com>
2025-03-30 15:11:03 -0300

    update

--- palindrome/main.c
+++ palindrome/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-#include "palin.h"
+#include "palindrome.h"
+#include <string.h>
+#include <ctype.h>
 
 int main() {
-   char str[80];
+   char str[81];
    printf("Enter your string: ");
-   if (
+   if(fgets(str, 81, stdin) == NULL) {
+     printf("Input error\n");
+     return 1;
+   }
+   str[strcspn(str, "\n")] = '\0';  // remove newline
+
+   int i;
+   for(i = 0; str[i] != '\0'; i++){
+     if (!isalpha(str[i]) && !isspace(str[i])){
+       printf("your string contains invalid character %c\n", str[i]);
+       return 1;
+     }
+   }
+
+   if ( str[0] == '\0') {
+     printf("your string is empty\n");
+     return 1;
+   }
+
+   return 0;
+}
--- palindrome/palindrome.c
+++ palindrome/palindrome.c
@@ -1,9 +1,35 @@
 #include "palindrome.h"
 #include <string.h>
+#include <ctype.h>
 
-int is_palindrome(const char *str){
-  int i = 0;
-  while(str[i]
+int is_palindrome(char s[]){
+    int i = 0;
+    int j = 0;
+    char str1[81];
+    for(i = 0; s[i] != '\0'; i++){
+        if(!isspace(s[i])){
+        str1[j++] = s[i];
+
+      }
+    }
+    str1[j] = '\0';
 
 
-}
+    j=strlen(str1);
+
+  int left = 0;
+  int right =j - 1;
+
+  while (left < right) {
+    if (str1[left] != str1[right]) {
+      return 0;
+    }
+    left++;
+    right--;
+  }
+  return 1;
+  }
+
+
+
+
